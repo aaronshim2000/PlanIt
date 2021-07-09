71dc2c2d27f280c5f74ce3b055a305209c0d67db
ajs30 <ajs30@sfu.ca>
2021-07-08 17:15:02 -0700

    Moved some files around

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -16,8 +16,6 @@
 
 package com.example;
 
-import com.Account;
-import com.AdminMessage;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
