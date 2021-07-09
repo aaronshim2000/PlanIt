a0d2afc88f50a425f50da6064dc7ba6ea9cefa3c
ajs30 <ajs30@sfu.ca>
2021-07-08 17:21:04 -0700

    Reverted previous change

---
+++ src/main/java/com/AdminMessage.java
@@ -0,0 +1,53 @@
+package com.example;
+
+enum Category {
+    CONTACT, REPORT
+}
+
+public class AdminMessage
+{
+    private String user;
+    private String email;
+    private String message;
+    private Category category;
+
+    public String getUser()
+    {
+        return this.user;
+    }
+
+    public String getEmail()
+    {
+        return this.email;
+    }
+
+    public String getMessage()
+    {
+        return this.message;
+    }
+
+    public Category getCategory()
+    {
+        return this.category;
+    }
+
+    public void setUser(String s)
+    {
+        user = s;
+    }
+
+    public void setEmail(String s)
+    {
+        email = s;
+    }
+
+    public void setMessage(String s)
+    {
+        message = s;
+    }
+
+    public void setCategory(Category c)
+    {
+        category = c;
+    }
+}
---
+++ src/main/java/com/Post.java
@@ -0,0 +1,42 @@
+package com.example;
+
+enum Category {
+    NORMAL, REVIEW, PLAN
+}
+
+public class Post
+{
+    private String title;
+    private String description;
+    private Category category;
+
+    public String getTitle()
+    {
+        return this.title;
+    }
+
+    public String getDescription()
+    {
+        return this.description;
+    }
+
+    public Category getCategory()
+    {
+        return this.category;
+    }
+
+    public void setTitle(String s)
+    {
+        title = s;
+    }
+
+    public void setDescription(String s)
+    {
+        description = s;
+    }
+
+    public void setCategory(Category c)
+    {
+        category = c;
+    }
+}
