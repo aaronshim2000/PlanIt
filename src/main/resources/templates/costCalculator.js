53117ca7a17ffb4d73ba7f49787b8f36179405b2
coronagi <85142380+coronagi@users.noreply.github.com>
2021-07-09 22:41:53 -0700

    Update costCalculator.js

--- src/main/resources/templates/costCalculator.js
+++ src/main/resources/templates/costCalculator.js
@@ -1,3 +1,18 @@
+function coladd() {
+        var table = document.getElementById("table");
+        var row = table.insertRow(-1);
+        var cell1 = row.insertCell(-1);
+        var cell2 = row.insertCell(-1);
+        var cell3 = row.insertCell(-1);
+        cell1.innerHTML = '<input type="text" name="fee" placeholder="Other fee">';
+        cell2.innerHTML = '<input type="text" name="price" placeholder="amount">';
+        cell3.innerHTML = '<input type="button" value="delete" id="coladd" onclick="coldel(this)">';
+    }
+    function coldel(obj) {
+        tr = obj.parentNode.parentNode;
+        tr.parentNode.deleteRow(tr.sectionRowIndex);
+    }
+
 function calculateTotal(){
  var priceArr=document.getElementsByName("price");
  var total=0;
