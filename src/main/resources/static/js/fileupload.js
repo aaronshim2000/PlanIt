a7d1a1b5aca0a8d974c823daced35f33bdbea088
GurbirSD <gurbirsd@gmail.com>
2021-07-23 15:17:36 -0700

    Image removal init test

--- src/main/resources/static/js/fileupload.js
+++ src/main/resources/static/js/fileupload.js
@@ -1,12 +1,61 @@
-var imgResults = document.getElementById("resultsP");
-var imgViewer1 = document.getElementById("viewImages1");
-var imgViewer2 = document.getElementById("viewImages2");
+var imgCell00 = document.getElementById("viewImg00");
+var imgCell01 = document.getElementById("viewImg01");
+var imgCell02 = document.getElementById("viewImg02");
+var imgCell03 = document.getElementById("viewImg03");
+var imgCell04 = document.getElementById("viewImg04");
+var imgCell05 = document.getElementById("viewImg05");
+var imgCell06 = document.getElementById("viewImg06");
+var imgCell07 = document.getElementById("viewImg07");
+var imgCell08 = document.getElementById("viewImg08");
+var imgCell09 = document.getElementById("viewImg09");
+
+var imgRes00 = document.getElementById("imageRes00");
+var imgRes01 = document.getElementById("imageRes01");
+var imgRes02 = document.getElementById("imageRes02");
+var imgRes03 = document.getElementById("imageRes03");
+var imgRes04 = document.getElementById("imageRes04");
+var imgRes05 = document.getElementById("imageRes05");
+var imgRes06 = document.getElementById("imageRes06");
+var imgRes07 = document.getElementById("imageRes07");
+var imgRes08 = document.getElementById("imageRes08");
+var imgRes09 = document.getElementById("imageRes09");
+
 var imageCounter = 0;
 
 function uploadImages(){
 	imageCounter = 0;
-	imgResults.innerHTML = '<input type="text" style="visibility: visible" id="imgRes" value="" disabled>';
+	imgCell00.style = "";
+	imgCell01.style = "";
+	imgCell02.style = "";
+	imgCell03.style = "";
+	imgCell04.style = "";
+	imgCell05.style = "";
+	imgCell06.style = "";
+	imgCell07.style = "";
+	imgCell08.style = "";
+	imgCell09.style = "";
+	imgRes00.value = "empty";
+	imgRes01.value = "empty";
+	imgRes02.value = "empty";
+	imgRes03.value = "empty";
+	imgRes04.value = "empty";
+	imgRes05.value = "empty";
+	imgRes06.value = "empty";
+	imgRes07.value = "empty";
+	imgRes08.value = "empty";
+	imgRes09.value = "empty";
 	imgWidget.open();
+}
+
+function removeImageNo( index ){
+	imageCounter--;
+	while (index < imageCounter){
+		document.getElementById("viewImg0" + index).style = document.getElementById("viewImg0" + (index+1)).style;
+		document.getElementById("imageRes0" + index).value = document.getElementById("imageRes0" + (index+1)).value;
+		index++;
+	}
+	document.getElementById("viewImg0" + imageCounter).style = "";
+	document.getElementById("viewImg0" + imageCounter).innerHTML = '<input type="text" th:field=*{image0' + imageCounter + '} id="imageRes0' + imageCounter + '" class="imageResult" value="empty" disabled>';
 }
 
 var imgWidget = cloudinary.createUploadWidget({
@@ -26,17 +75,11 @@ var imgWidget = cloudinary.createUploadWidget({
   (error, result) => { 
     if (!error && result && result.event === "success") {
 	  var newRes = result.info;
-	  var domDimension = 'width';
-	  if (newRes.width < newRes.height){
-		domDimension = 'height';
-	  }
-      imgResults.innerHTML = imgResults.innerHTML + '<input type="text" style="visibility: visible" id="imgRes' + imageCounter + '" value="' + newRes.url + '" disabled>';
-	  if (imageCounter < 5){
-		  imgViewer1.innerHTML = imgViewer1.innerHTML + '<img src="' + newRes.url + '" id="viewImg0' + imageCounter + '" ' + domDimension + '="250">'
-	  }
-	  else{
-		  imgViewer2.innerHTML = imgViewer2.innerHTML + '<img src="' + newRes.url + '" id="viewImg0' + imageCounter + '" ' + domDimension + '="250">'
-	  }
+	  var currCell = document.getElementById("viewImg0" + imageCounter);
+	  var currRes = document.getElementById("imageRes0" + imageCounter);
+	  currCell.style = "width: 250px; height: 250px; background-image: url('" + newRes.url + ");";
+	  currRes.value = newRes.url;
+	  currCell.innerHTML = '<button type="button" id="removeImg0' + imageCounter + '" onclick="removeImageNo(' + imageCounter + ')" class="removeButton">X</button>' + currCell.innerHTML;
 	  imageCounter++;
     }
   }
