624c6b5be56dac28fed99ff702240657dfd9cfa7
GurbirSD <gurbirsd@gmail.com>
2021-07-23 15:25:56 -0700

    Moved file uploading style to css

--- src/main/resources/static/js/fileupload.js
+++ src/main/resources/static/js/fileupload.js
@@ -86,16 +86,3 @@ var imgWidget = cloudinary.createUploadWidget({
 )
 
 document.getElementById("upload_widgetImage").addEventListener("click", function(){uploadImages()});
-
-/*var cl = new cloudinary.Cloudinary({cloud_name: "planit276", secure: true});
-var i00s = document.getElementById("image00s");
-
-i00s.addEventListener("change", function(){uploadI( 0 )});
-
-function uploadI( i ){
-	var uploadedI = document.getElementById("image0" + i + "s");
-	var uIValue = uploadedI.value;
-	cloudinary.v2.uploader.upload(uIValue,
-  { public_id: "planit_image" }, 
-  function(error, result) {console.log(result); });
-}*/
