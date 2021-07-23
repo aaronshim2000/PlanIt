de7ef7fe9f82a4b55e9810a8541bc6da0e62bfb9
GurbirSD <gurbirsd@gmail.com>
2021-07-22 21:53:31 -0700

    Upload widget test deploy

--- src/main/resources/static/js/fileupload.js
+++ src/main/resources/static/js/fileupload.js
@@ -1,4 +1,21 @@
-var cl = new cloudinary.Cloudinary({cloud_name: "planit276", secure: true});
+var myWidget = cloudinary.createUploadWidget({
+  cloudName: 'hq73wefct', 
+  uploadPreset: 't4vy5bss'}, (error, result) => { 
+    if (!error && result && result.event === "success") { 
+      console.log('Done! Here is the image info: ', result.info); 
+    }
+  }
+)
+
+document.getElementById("upload_widget").addEventListener("click", function(){
+	myWidget.open();
+}, false);
+
+var myCropWidget = cloudinary.createUploadWidget({
+  cloudName: 'demo', uploadPreset: 'preset1', folder: 'widgetUpload', cropping: true}, 
+  (error, result) => { console.log(error, result) })
+
+/*var cl = new cloudinary.Cloudinary({cloud_name: "planit276", secure: true});
 var i00s = document.getElementById("image00s");
 
 i00s.addEventListener("change", function(){uploadI( 0 )});
@@ -9,4 +26,4 @@ function uploadI( i ){
 	cloudinary.v2.uploader.upload(uIValue,
   { public_id: "planit_image" }, 
   function(error, result) {console.log(result); });
-}
+}*/
