96ba5837bd551535679c20451ac5ed170996c318
coronagi <paws.field2@gmail.com>
2021-08-01 19:06:28 -0700

    search function from title/content/username

--- src/main/resources/static/js/scrollingFeed.js
+++ src/main/resources/static/js/scrollingFeed.js
@@ -4,23 +4,32 @@ let filter=document.getElementById("filter").value.toUpperCase();
 let PlanPostRecord=document.getElementsByClassName("Record-Plan-post");
 for(var i=0;i<PlanPostRecord.length;i++){
  let title=PlanPostRecord[i].getElementsByTagName("p")[0];
- if(title){
-   let titleValue=title.textContent;
-   if(titleValue.toUpperCase().indexOf(filter)>-1){
-      PlanPostRecord[i].style.display="";
+ let content=PlanPostRecord[i].getElementsByTagName("p")[1];
+ let user=PlanPostRecord[i].getElementsByTagName("p")[2];
+
+   if(title||content||user){
+     let titleValue=title.textContent;
+     let contentValue=content.textContent;
+     let userValue=user.textContent;
+     if(titleValue.toUpperCase().indexOf(filter)>-1||contentValue.toUpperCase().indexOf(filter)>-1||userValue.toUpperCase().indexOf(filter)>-1){
+        PlanPostRecord[i].style.display="";
+     }
+     else{
+        PlanPostRecord[i].style.display="none";
+     }
    }
-   else{
-      PlanPostRecord[i].style.display="none";
-   }
- }
 }
 
 let ReviewPostRecord=document.getElementsByClassName("Record-Review-post");
 for(var i=0;i<ReviewPostRecord.length;i++){
   let title=ReviewPostRecord[i].getElementsByTagName("p")[0];
-    if(title){
+  let content=ReviewPostRecord[i].getElementsByTagName("p")[2];
+  let user=ReviewPostRecord[i].getElementsByTagName("p")[3];
+    if(title||content||user){
      let titleValue=title.textContent;
-      if(titleValue.toUpperCase().indexOf(filter)>-1){
+     let contentValue=content.textContent;
+     let userValue=user.textContent;
+     if(titleValue.toUpperCase().indexOf(filter)>-1||contentValue.toUpperCase().indexOf(filter)>-1||userValue.toUpperCase().indexOf(filter)>-1){
         ReviewPostRecord[i].style.display="";
       }
      else{
@@ -32,9 +41,13 @@ for(var i=0;i<ReviewPostRecord.length;i++){
 let TextPostRecord=document.getElementsByClassName("Record-Text-post");
 for(var i=0;i<TextPostRecord.length;i++){
  let title=TextPostRecord[i].getElementsByTagName("p")[0];
- if(title){
+ let content=TextPostRecord[i].getElementsByTagName("p")[1];
+ let user=TextPostRecord[i].getElementsByTagName("p")[2];
+ if(title||content||user){
    let titleValue=title.textContent;
-   if(titleValue.toUpperCase().indexOf(filter)>-1){
+   let contentValue=content.textContent;
+   let userValue=user.textContent;
+   if(titleValue.toUpperCase().indexOf(filter)>-1||contentValue.toUpperCase().indexOf(filter)>-1||userValue.toUpperCase().indexOf(filter)>-1){
       TextPostRecord[i].style.display="";
    }
    else{
