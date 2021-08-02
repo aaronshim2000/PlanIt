4db722c5636597ee1746948b09a73f2efb4bf008
GurbirSD <gurbirsd@gmail.com>
2021-08-02 12:07:36 -0700

    Video data saving test

--- src/main/resources/public/videoplay.js
+++ src/main/resources/public/videoplay.js
@@ -6,5 +6,6 @@ var demoplayer = cld.videoPlayer('demo-player', {
 	bigPlayButton: 'init',
 	controls: true,
 	showLogo: false,
-	loop: true
+	loop: true,
+	preload: 'auto'
 });
--- src/main/resources/public/videoupload.js
+++ src/main/resources/public/videoupload.js
@@ -1,3 +1,15 @@
+console.log("loaded ur js");
+
+var cld = cloudinary.Cloudinary.new({ cloud_name: "hq73wefct", secure: true});
+
+var demoplayer = cld.videoPlayer('demo-player', {
+	bigPlayButton: 'init',
+	controls: true,
+	showLogo: false,
+	loop: true
+	preload: 'auto'
+});
+
 function uploadImages(){
 	imgWidget.open();
 }
@@ -19,6 +31,8 @@ var imgWidget = cloudinary.createUploadWidget({
 	  console.log(result.info);
 	  var newRes = result.info;
 	  console.log(newRes.url);
+	  console.log(newRes.public_id);
+	  demoplayer.source(newRes.public_id);
     }
   }
 )
