a96a9356d457a708676f898899ad464bc3d0bc45
GurbirSD <gurbirsd@gmail.com>
2021-08-06 16:53:15 -0700

    Fixing some viewPost sizing issues

--- src/main/resources/static/js/viewPost.js
+++ src/main/resources/static/js/viewPost.js
@@ -4,7 +4,7 @@ var videoID = document.getElementById("video00").value;
 var myMax = 0;
 if (document.getElementById("imagesMax").value == "text-post"){ myMax = 10; }
 if (document.getElementById("imagesMax").value == "review-post"){ myMax = 5; }
-document.getElementById("imNumContain").innerHTML = '';
+//document.getElementById("imNumContain").innerHTML = '';
 var playerOnPage = document.getElementById("containerForVid");
 
 var cld = cloudinary.Cloudinary.new({ cloud_name: "hq73wefct", secure: true});
@@ -31,4 +31,9 @@ else if (mediaType == "video"){
 	playerOnPage.innerHTML = '<video id="demo-player" controls class="cld-video-player HiddenPlayer"></video>';
 	enablePlayer();
 	demoplayer.source( videoID );
+}
+else{
+	for (let i = 0; i < myMax; i++){
+		document.getElementById("image0" + i).innerHTML = '';
+	}
 }
