9639900077d75eaef5f69513418f59c38993aa8d
GurbirSD <gurbirsd@gmail.com>
2021-08-06 11:59:09 -0700

    Just a quick to get my wip files in the git

---
+++ src/main/resources/templates/fileViewingTest.js
@@ -0,0 +1,35 @@
+var vidRes00 = document.getElementById("videoPubID");
+
+var cld = cloudinary.Cloudinary.new({ cloud_name: "hq73wefct", secure: true});
+
+function enablePlayer(){
+	demoplayer = cld.videoPlayer('demo-player', {
+		bigPlayButton: 'init',
+		controls: true,
+		showLogo: false,
+		loop: true,
+		preload: 'auto'
+	});
+}
+
+function displayMedia( postID, mType, imgAmmount ){
+	console.log("Called display from post " + postID);
+	currPost = document.getElementById(postID);
+	aFiles = currPost.getElementsByClassName("imageCell");
+	if (mType == "images"){
+		if (imgAmmount == 1){
+			currPost.innerHTML = '<img src="' + aFiles[0].innerHTML + '" class="postImage" />'
+		}
+		else{
+			for (let i = 0; i < imgAmmount; i++){
+				
+			}
+		}
+	}
+	else if (mType == "video"){
+		
+	}
+	else{
+		currPost.innerHTML = '';
+	}
+}
